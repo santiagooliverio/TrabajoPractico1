e43dfe80dde486828f6a6db4161bc1dd3166d821
santiagooliverio <oliveriosantiago006@gmail.com>
2021-09-25 21:50:20 -0300

    TP finalizado

---
+++ TP_[1]/src/Funciones.h
@@ -0,0 +1,28 @@
+/*
+ * Funciones.h
+ *
+ *  Created on: 25 sep. 2021
+ *      Author: Santiago
+ */
+
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+/// @fn int suma(int, int)
+/// @brief suma de dos numeros enteros
+/// @param a numero entero
+/// @param b numero entero
+/// @return resultado de la suma de los dos enteros
+
+int suma(int a, int b);
+
+int resta(int a, int b);
+
+int multiplicacion(int a, int b);
+
+float division(int a, int b);
+
+int factorial(int a);
+
+int menu(int operacion, int a, int b);
+
+#endif /* FUNCIONES_H_ */
